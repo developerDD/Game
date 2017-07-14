6724beaff096930c90bc3e8541a10d1d1a4f31e2
developerDD <developerDD@ukr.net>
2017-07-14 10:14:51 +0300

    вер 1

--- CardGame/CardGame/Program.cs
+++ CardGame/CardGame/Program.cs
@@ -11,7 +11,7 @@ namespace CardGame
         class Karta : IComparable<Karta>
         {
             public string velu { set; get; }
-            public string mast {set; get; }
+            public string mast { set; get; }
 
             public int ves { set; get; }
 
@@ -24,7 +24,7 @@ namespace CardGame
 
             public int CompareTo(Karta obj)
             {
-                if (this.ves>obj.ves)
+                if (this.ves > obj.ves)
                 {
                     return 1;
                 }
@@ -41,48 +41,48 @@ namespace CardGame
 
         class KolodaKard
         {
-          public  List<Karta> koloda { set; get; }
-            string[] mas = { "чирва","буба","креста","пика" };
-            string[] mas1 = { "6", "7", "8", "9","10", "Валет", "Дама", "Король", "Туз" };
+            public List<Karta> koloda { set; get; }
+            string[] mas = { "чирва", "буба", "креста", "пика" };
+            string[] mas1 = { "6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз" };
 
-           public KolodaKard()
+            public KolodaKard()
             {
                 koloda = new List<Karta>(36);
                 for (int i = 0; i < 4; i++)
                 {
                     for (int j = 0; j < mas1.Length; j++)
                     {
-                        koloda.Add(new Karta(mas1[j], mas[i],j));
+                        koloda.Add(new Karta(mas1[j], mas[i], j));
                     }
                 }
                 Rend_koloda();
-             }
-           void Rend_koloda()
-           {
-               Random rnd = new Random();
-               for (int i = 0; i < koloda.Count; i++)
-               {
-                   Karta t = koloda[0];
-                   koloda.RemoveAt(0);
-                   koloda.Insert(rnd.Next(koloda.Count), t);
-               }
-           }
-         }
+            }
+            void Rend_koloda()
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < koloda.Count; i++)
+                {
+                    Karta t = koloda[0];
+                    koloda.RemoveAt(0);
+                    koloda.Insert(rnd.Next(koloda.Count), t);
+                }
+            }
+        }
 
-        class Playr 
+        class Playr
         {
             public List<Karta> kartu { set; get; }
 
-           public Playr()
+            public Playr()
             {
                 kartu = new List<Karta>();
             }
 
             public int Srav(Playr obj)
-           {
-               return this.kartu[0].CompareTo(obj.kartu[0]);
-           }
-           
+            {
+                return this.kartu[0].CompareTo(obj.kartu[0]);
+            }
+
         }
 
         static void Main(string[] args)
@@ -90,9 +90,9 @@ namespace CardGame
             KolodaKard k = new KolodaKard();
             Playr dima = new Playr();
             for (int i = 0; i < 12; i++)
-			{
+            {
                 dima.kartu.Add(k.koloda[i]);
-			}
+            }
 
 
 
@@ -101,15 +101,8 @@ namespace CardGame
                 Console.WriteLine(item.mast + " " + item.velu + " " + item.ves);
             }
 
-            foreach (var item in k.koloda)
-            {
-                Console.WriteLine(item.mast + " " + item.velu + " " + item.ves);
-            } 
-            foreach (var item in k.koloda)
-        {
-           Console.WriteLine(item.mast+" "+item.velu+" "+item.ves);
-        }  
-            
+
         }
     }
+
 }
